8a27df7d21ef1cc061dcf41e8f069e0cbbe42d2d
cybernaut4 <arkl1te@protonmail.com>
2025-07-05 19:08:19 +0200

    v23 fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/game-dev-portfolio',
+  assetPrefix: '/game-dev-portfolio/',
   eslint: {
     ignoreDuringBuilds: true,
   },
